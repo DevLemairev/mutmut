748e183cfd459d83c16d1fcec071fe9f6ad2feb6
Lemaire Vincent <dev.lemairev@gmail.com>
2022-09-16 13:46:45 +0200

    Fix some css

--- src/component/Board.js
+++ src/component/Board.js
@@ -5,6 +5,10 @@ import '../style/board.css'
 import { useEffect, useState } from 'react'
 import word from '../data/word.js'
 
+export const GAME_STEP_ONGOING = 'ONGOING'
+export const GAME_STEP_WIN = 'WIN'
+export const GAME_STEP_LOOSE = 'LOOSE'
+
 function Board() {
 
   const [ rows, setRows ] = useState(
@@ -21,20 +25,20 @@ function Board() {
   );
   const [ currentRowIndex, setCurrentRowIndex ] = useState(0)
   const [ currentLetterIndex, setCurrentLetterIndex ] = useState(1)
-  const [ gameStep, setGameStep ] = useState('CONTINUE')
+  const [ gameStep, setGameStep ] = useState(GAME_STEP_ONGOING)
 
   function onLetterPressed(letter) {
     const newRows = rows.slice()
     const currentRow = newRows[currentRowIndex]
-    if (letter === MM_KB_BACKSPACE) {
+    if (letter === MM_KB_BACKSPACE) /* Player clean current cell in grid */ {
       currentRow[currentLetterIndex] = { letter: '.', state: MM_LETTER_STATE_TBD }
       if (currentLetterIndex > 1) {
         setCurrentLetterIndex(currentLetterIndex - 1)
       }
-    } else if (letter === MM_KB_RETURN) {
-      if (currentRow.find(cell => cell.letter === '.') !== undefined) {
+    } else if (letter === MM_KB_RETURN) /* Player submit current row in grid (eg submit a word) */{
+      if (currentRow.find(cell => cell.letter === '.') !== undefined) /* Word is not complete */ {
         alert("Le mot n'est pas complet !")
-      } else {
+      } else /* Word is complete : check which letters are found, at the right place or not */ {
         let wordFetched = word
         for(let cellIndex = 0; cellIndex < currentRow.length; cellIndex++) {
           const cell = currentRow[cellIndex]
@@ -52,13 +56,13 @@ function Board() {
           cell.state = MM_LETTER_STATE_NOTINWORD
         }
         if (currentRow.find(cell => cell.state !== MM_LETTER_STATE_INWORD_INPLACE) === undefined) {
-          setGameStep('WIN')
+          setGameStep(GAME_STEP_WIN)
         } else if (currentRowIndex < newRows.length - 1) {
           newRows[currentRowIndex + 1][0] = { letter: word[0], state: MM_LETTER_STATE_INWORD_INPLACE }
           setCurrentRowIndex(currentRowIndex + 1)
           setCurrentLetterIndex(1)
         } else {
-          setGameStep('LOOSE')
+          setGameStep(GAME_STEP_LOOSE)
         }
       }
     } else {
@@ -71,9 +75,9 @@ function Board() {
   }
 
   useEffect(() => {
-    if (gameStep === 'WIN') {
+    if (gameStep === GAME_STEP_WIN) {
       alert("Bravo !")
-    } else if (gameStep === 'LOOSE') {
+    } else if (gameStep === GAME_STEP_LOOSE) {
       alert("Perdu : la solution est " + word)
     }
   })
@@ -83,7 +87,7 @@ function Board() {
       <Header />
       <main id="mm-main">
         <Grid rows={rows} currentRowIndex={currentRowIndex} currentCellIndex={currentLetterIndex}/>
-        <Keyboard enabled={gameStep === 'CONTINUE'} onLetterPressed={onLetterPressed} />
+        <Keyboard enabled={gameStep === GAME_STEP_ONGOING} onLetterPressed={onLetterPressed} />
       </main>
     </div>
   )
